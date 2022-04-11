3a827f1b7b4616d70e3b23cd6f9c0f8a79c7aa96
peder frang <peder.frang@gmail.com>
2022-04-11 14:08:38 +0200

    feat_refactoring_abit_plus_css

--- src/componenents/Container.jsx
+++ src/componenents/Container.jsx
@@ -1,54 +1,41 @@
 import React, { useState } from 'react'
 import { openAICall } from '../api-client/openaiclient'
-import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
-// import { Select } from 'native-base';
+import Header from './Header';
+import Dropdown from 'react-dropdown';
 // import api from '../api-client/azure_func/HttpTrigger1'
-// import DropDownPicker from 'react-native-dropdown-picker';
 export default function Container() {
 
-  const [submitText, setSubmitText] = useState();
+  const [response, setResponse] = useState('');
   const [isLoading, setisLoading] = useState(false);
+  const [dropdown, setDropdown] = useState('')
   const waitFunc = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
+  const handleDropdown = (e) => {
+    setDropdown(e.value);
+  }
+
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    // const input = e.currentTarget.children[0].value
-    const input = e.target.firstChild.lastChild.outerText
-    console.log(input)
-    // console.log(input)
-    // console.log(e.target.children[0].value)
+    if(!dropdown) {
+      return alert("Please select an option from the dropdown menu!")
+    }
     setisLoading(true)
-    // const call = await openAICall(input)
-
+    // const call = await openAICall(dropdown)
+    // setResponse(call);
     await waitFunc(500)
-
     const text = "This is a test string string This is a test string string  This is a test string string  This is a test string string  "
-    setSubmitText(text);
+    setResponse(text);
     setisLoading(false);
-    // console.log(submitText);
   }
   return (
-    <div id="wrapper">
-      <div>
-        <h1>Welcome to lars and Peder's drinking game!</h1>
-        <form className="grid grid-cols-2" onSubmit={handleSubmit}>
-          <Dropdown options={["Rule", "Dilemma", "Trivia Question", "Pointing Game Question"]} />
-          {/* <select name="languages" id="lang">
-            <option value="Rule:">Rule:</option>
-            <option value="Dilemma:">Dilemma</option>
-            <option value="Trivia Question:">Trivia Question</option>
-            <option value="Pointing Game Question:">Pointing Game Question</option>
-          </select> */ }
-          <input className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" type="submit" />
-        </form>
-      </div>
-      <div id="div-output">
-        {isLoading ? <h2>...Loading</h2> : <h2 className="font-bold drop-shadow-lg" id="output">{submitText ? submitText : ""}</h2>}
-      </div>
-
+    <>
+    <Header/>
+    <Dropdown onChange={handleDropdown} className='w-1/4 m-auto my-4' options={["Rule", "Dilemma", "Trivia Question", "Pointing Game Question"]} />
+    <div id="wrapper" className='flex justify-center items-center' onClick={handleSubmit}>
+        {isLoading ? <h2>...Loading</h2> : <h2 className="font-bold drop-shadow-lg" id="output">{response ? response : ""}</h2>}
     </div>
+    </>
   )
 }
---
+++ src/componenents/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dropdown from 'react-dropdown';
+
+
+function Header(props) {
+  return (
+    <header>
+      <h1>Welcome to lars and Peder's drinking game!</h1>
+    </header>
+  );
+}
+
+export default Header;
