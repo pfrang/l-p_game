c47db565ef71e9a80e1f465f6861a66a80107ef7
Peder Frang <peder.frang@gmail.com>
2022-04-12 21:10:46 +0200

    feat_added_prior_questions_to_state_incrementing

--- src/componenents/Container.jsx
+++ src/componenents/Container.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { openAICall } from '../api-client/openaiclient'
 import 'react-dropdown/style.css';
 import Header from './Header';
 import Dropdown from 'react-dropdown';
-// import api from '../api-client/azure_func/HttpTrigger1'
 export default function Container() {
 
   const [response, setResponse] = useState('');
   const [isLoading, setisLoading] = useState(false);
   const [dropdown, setDropdown] = useState('')
+  const [questions, setQuestions] = useState([]);
+  const [questionIncrementer, setQuestionIncrementer] = useState(0);
+
+
   const waitFunc = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
+  const setLocalStorage = () => {
+    window.sessionStorage.setItem('questions', questions);
+  }
+
   const handleDropdown = (e) => {
     setDropdown(e.value);
+  }
+
+  const handlePriorQuestion = async () => {
+    if (!dropdown) {
+      return alert("Please select an option from the dropdown menu!")
+    }
+    if(questionIncrementer === 0 || questionIncrementer === 1 ) {
+      return
+    }
+    console.log(questions[questionIncrementer - 1]);
+    setResponse(questions[questionIncrementer - 2]);
+    setQuestionIncrementer(questionIncrementer - 1);
   }
 
   const handleNextQuestion = async (e) => {
@@ -22,21 +41,30 @@ export default function Container() {
       return alert("Please select an option from the dropdown menu!")
     }
     setisLoading(true)
-    // const call = await openAICall(dropdown)
-    // setResponse(call);
     // Uncomment below for testing and comment two lines above
-    await waitFunc(500)
-    const call = "This is a test string string This is a test string string  This is a test string string  This is a test string string  "
-    setResponse(call);
+    if(questions.length === questionIncrementer) {
+      // const call = await openAICall(dropdown)
+      // setResponse(call);
+      await waitFunc(500)
+      const timestamp = new Date().toTimeString().split(' ')[0]
+      const call = timestamp
+      setResponse(call);
+      setQuestions(questions => [...questions, call]);
+    } else {
+
+      setResponse(questions[questionIncrementer]);
+    }
+    setQuestionIncrementer(questionIncrementer + 1);
     setisLoading(false);
   }
+
   return (
     <>
       <Header />
       <Dropdown onChange={handleDropdown} className='w-1/4 m-auto my-4' options={["Rule", "Dilemma", "Trivia Question", "Pointing Game Question"]} />
       {isLoading ? <h2>...Loading</h2> : <h2 className="font-bold drop-shadow-lg" id="output">{response ? response : ""}</h2>}
       <div id="wrapper">
-        <div >
+        <div onClick={handlePriorQuestion}>
         </div>
         <div onClick={handleNextQuestion}>
         </div >
